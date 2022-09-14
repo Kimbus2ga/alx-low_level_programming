8e6cee449750fbe0c8d3ab6a7bc118fadf31a8ab
Kimbus2ga <111077919+Kimbus2ga@users.noreply.github.com>
2022-09-14 06:08:54 +0100

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+es (17 sloc)  221 Bytes
+
+#include "main.h"
+
+/**
+ * _islower - Short decription, single line
+ *@c: contains value to be comapred
+ *Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+
+}
